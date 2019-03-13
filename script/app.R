c853050270c8c74a0d0dcfdf3ed9ec10e007b13e
YUKI ONO <yuki.spring21.tennis@gmail.com>
2019-03-13 04:23:55 -0700

    complete my part

--- script/app.R
+++ script/app.R
@@ -3,6 +3,8 @@ library("shiny")
 library("ggplot2")
 library("dplyr")
 library("tidyr")
+library("lubridate")
+library("stringr")
 
 # You will once again be working with the `diamonds` data set provided by ggplot2
 # Use dplyr's `sample_n()` function to get a random 3000 rows from the data set
@@ -27,6 +29,15 @@ ui <- fluidPage(
   # A `titlePanel` with the title "Diamond Viewer"
   titlePanel("zillow home price"),
   
+  sidebarLayout(
+    sidebarPanel(
+    sliderInput("date_range", 
+                "Choose Date Range:", 
+                min = as.Date("1996-04-01"),
+                max = as.Date("2019-01-01"),
+                value = c(as.Date("1996-04-01"), as.Date("2019-01-01"))
+    ),
+  
   # A `selectInput()` labeled "select your state". This dropdown should let
   # the user pick one of the states. 
   selectInput("state",
@@ -42,12 +53,15 @@ ui <- fluidPage(
   # the user pick one of the state. 
   selectInput("state_two",
               label = "Select another state",
-              choices = select_values
-  ),
+              choices = select_values)
+    ),
+  mainPanel(
   
   # A plotOutput showing the 'plot' output (based on the user specifications)
   plotOutput("plot")
+  )
   
+)
 )
 
 # Define a `server` function (with appropriate arguments)
@@ -57,10 +71,10 @@ server <- function(input, output){
     data <- one %>%
       filter(RegionName == input$state) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "1")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     
     data #return data
   })
@@ -68,94 +82,105 @@ server <- function(input, output){
     data <- two %>%
       filter(RegionName == input$state) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "2")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   gathered_three <- reactive({
     data <- three %>%
       filter(RegionName == input$state) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "3")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   gathered_four <- reactive({
     data <- four %>%
       filter(RegionName == input$state) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "4")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   gathered_fiveplus <- reactive({
     data <- five_or_more %>%
       filter(RegionName == input$state) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "5+")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2])) 
     data #return data
+  })
+  
+  data_combined <- reactive({
+    data <- rbind(gathered_one(), gathered_two(), gathered_three(),
+                         gathered_four(), gathered_fiveplus())
+    data
   })
   
   another_one <- reactive({
     data <- one %>%
       filter(RegionName == input$state_two) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "compared 1")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   another_two <- reactive({
     data <- two %>%
       filter(RegionName == input$state_two) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "compared 2")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   another_three <- reactive({
     data <- three %>%
       filter(RegionName == input$state_two) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "compared 3")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   another_four <- reactive({
     data <- four %>%
       filter(RegionName == input$state_two) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "compared 4")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   another_fiveplus <- reactive({
     data <- five_or_more %>%
       filter(RegionName == input$state_two) %>%
       gather(key = "year_month", value = "price", -1, -2, -3) %>%
-      select(year_month, price)
-    data$year_month <- str_sub(data$year_month, start = 2)
-    data$year_month <- gsub("[.]", "-", data$year_month)
-    data$year_month <- as.Date(parse_date_time(data$year_month, "Y-m"))
+      select(year_month, price) %>%
+      mutate(bedroom = "compared 5+")
+    data$year_month <- as.Date(parse_date_time(gsub("[.]", "-", str_sub(data$year_month, start = 2)), "Y-m"))
+    data <- filter(data, year_month >= as.Date(input$date_range[1]), year_month <= as.Date(input$date_range[2]))
     data #return data
   })
   
+  data_combined_compared <- reactive({
+    data <- rbind(another_one(), another_two(), another_three(),
+                  another_four(), another_fiveplus())
+    data
+  })
   # Assign a reactive `renderPlot()` function to the outputted 'plot' value
   output$plot <- renderPlot({
     
@@ -167,25 +192,15 @@ server <- function(input, output){
     # Facet the plot based on which feature the user selected to "facet by"
     #   (hint: you can just pass that string directly to `facet_wrap()`)
     # Save your plot as a variable.
-    p <- ggplot() +
-      geom_point(data = gathered_one(), mapping = aes_string(x = "year_month", y = "price"), color = "cornflowerblue") +
-      geom_point(data = gathered_two(), mapping = aes_string(x = "year_month", y = "price"), color = "turquoise2") +
-      geom_point(data = gathered_three(), mapping = aes_string(x = "year_month", y = "price"), color = "turquoise3") +
-      geom_point(data = gathered_four(), mapping = aes_string(x = "year_month", y = "price"), color = "turquoise4") +
-      geom_point(data = gathered_fiveplus(), mapping = aes_string(x = "year_month", y = "price"), color = "forestgreen")
-      
-    
+    p <- ggplot(data = data_combined()) +
+      geom_point(mapping = aes(x = year_month, y = price, color = bedroom)) + 
+      scale_color_brewer()
     # Finally, if the "trendline" checkbox is selected, you should also include 
     # a geom_smooth geometry (with `se=FALSE`)
     # Hint: use an if statement to see if you need to add more geoms to the plot
     # Be sure and return the completed plot!
     if (input$compare) {
-      p <- p +  geom_point(data = another_one(), mapping = aes_string(x = "year_month", y = "price"), color = "darkorchid1") +
-        geom_point(data = another_two(), mapping = aes_string(x = "year_month", y = "price"), color = "darkorchid2") +
-        geom_point(data = another_three(), mapping = aes_string(x = "year_month", y = "price"), color = "darkorchid3") +
-        geom_point(data = another_four(), mapping = aes_string(x = "year_month", y = "price"), color = "darkorchid4") +
-        geom_point(data = another_fiveplus(), mapping = aes_string(x = "year_month", y = "price"), color = "darkred") 
-      
+      p <- p + geom_point(data = data_combined_compared(), mapping = aes(x = year_month, y = price), alpha = 0.1) 
     }
     
     p # return the plot
